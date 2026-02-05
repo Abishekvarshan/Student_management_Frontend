5204b519eda1765febdac5cd082bbf46cf2fb736
Abishekvarshan <varsityabi@gmail.com>
2026-02-05 13:12:20 +0530

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Welcome to Student Management Frontend</h1>
+      <p>Next.js App Router is working 🚀</p>
+    </div>
+  );
+}
